94478b44eb2cbd34255f271c33589e87934c8fbd
YeongJae0114 <yjl8628@naver.com>
2024-10-09 16:29:05 +0900

    ⚡️ 대본을 전달하는 DTO

---
+++ src/main/java/TestAI/openAI/script/dto/CreateVideoDto.java
@@ -0,0 +1,11 @@
+package TestAI.openAI.script.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateVideoDto {
+    private String articleId;
+    private String shortFormScript;
+}
