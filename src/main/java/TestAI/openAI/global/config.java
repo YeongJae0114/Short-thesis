24334a441e95f6fd18c42cba0dcc0cf5da7b2a5d
YeongJae0114 <yjl8628@naver.com>
2024-11-19 13:58:18 +0900

    Swagger 의존성 추가, 등록 - API 문서를 생성

--- src/main/java/TestAI/openAI/global/config.java
+++ src/main/java/TestAI/openAI/global/config.java
@@ -1,5 +1,8 @@
 package TestAI.openAI.global;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -12,4 +15,20 @@ public class config {
                 .baseUrl("https://widely-select-polliwog.ngrok-free.app/")
                 .build();
     }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Swagger")
+                .description("REST API")
+                .version("1.0.0");
+    }
+
+
 }
